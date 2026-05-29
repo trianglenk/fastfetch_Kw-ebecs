92f4b51ea88a2f34d979988592ceeb469badac86
TriangleNk <nikitaserdukov55@gmail.com>
2026-05-29 23:53:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.config/fastfetch
 
 # Копируем файлы из репозитория
 cp fastfetch/config.jsonc ~/.config/fastfetch/
-cp fastfetch/logo.png ~/.config/fastfetch/
+cp .config/fastfetch/logo.png ~/.config/fastfetch/
 
 # Исправляем путь к логотипу в конфиге
 sed -i "s|\"source\": \".*\"|\"source\": \"$HOME/.config/fastfetch/logo.png\"|" ~/.config/fastfetch/config.jsonc
